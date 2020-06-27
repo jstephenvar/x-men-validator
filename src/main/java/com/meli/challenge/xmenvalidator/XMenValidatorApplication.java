342a1be78356faa1aadc05e0f28eae5762fa14f6
Johan Vargas <johan.vargas@globant.com>
2020-06-27 13:26:14 -0500

    Initial setup adding as a spring boot project.

---
+++ src/main/java/com/meli/challenge/xmenvalidator/XMenValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.meli.challenge.xmenvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XMenValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XMenValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/challenge/xmenvalidator/XMenValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.challenge.xmenvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XMenValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
