26fe7864427643789e38fa3603cbec6d1e9c171a
Johan Vargas <johan.vargas@globant.com>
2020-06-29 10:52:48 -0500

    Add secret manager of AWS, resolve gap from keys over config file.

--- src/main/java/com/meli/challenge/xmenvalidator/config/DynamoConfig.java
+++ src/main/java/com/meli/challenge/xmenvalidator/config/DynamoConfig.java
@@ -7,22 +7,43 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.DecryptionFailureException;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.amazonaws.services.secretsmanager.model.InternalServiceErrorException;
+import com.amazonaws.services.secretsmanager.model.InvalidRequestException;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import lombok.extern.log4j.Log4j2;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.security.InvalidParameterException;
+
+import static com.meli.challenge.xmenvalidator.general.Constants.ERROR_SECRET_DECRYPT;
+import static com.meli.challenge.xmenvalidator.general.Constants.ERROR_SECRET_INVALID_PARAM;
+import static com.meli.challenge.xmenvalidator.general.Constants.ERROR_SECRET_REQUEST;
+import static com.meli.challenge.xmenvalidator.general.Constants.ERROR_SECRET_RESOURCE_NOT_FOUND;
+import static com.meli.challenge.xmenvalidator.general.Constants.ERROR_SECRET_SERVICE_FAIL;
+
+@Log4j2
 @Configuration
 @EnableDynamoDBRepositories
         (basePackages = "com.meli.challenge.xmenvalidator.repository")
 public class DynamoConfig {
     
     
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
-    
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
+    private static final String KEY = "accesskey";
+    private static final String SECRET = "secretkey";
+    @Value("${amazon.aws.dynamo}")
+    private String secretName;
+    @Value("${amazon.aws.region}")
+    private String region;
     
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
@@ -34,8 +55,55 @@ public class DynamoConfig {
     
     @Bean
     public AWSCredentials amazonAWSCredentials() {
+        JsonObject jsonObject = getSecret();
         return new BasicAWSCredentials(
-                amazonAWSAccessKey, amazonAWSSecretKey);
+                jsonObject.get(KEY).getAsString(), jsonObject.get(SECRET).getAsString());
+    }
+    
+    /**
+     * Get secret keys for connection to dynamo from AWS secret.
+     *
+     * @return JSONObject keys
+     */
+    private JsonObject getSecret() {
+        
+        // Create a Secrets Manager client
+        AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard()
+                .withRegion(region)
+                .build();
+        
+        String secret = "";
+        
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+                .withSecretId(this.secretName);
+        GetSecretValueResult getSecretValueResult = null;
+        
+        try {
+            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+        } catch (DecryptionFailureException e) {
+            log.error(ERROR_SECRET_DECRYPT, e);
+            throw e;
+        } catch (InternalServiceErrorException e) {
+            log.error(ERROR_SECRET_SERVICE_FAIL, e);
+            throw e;
+        } catch (InvalidParameterException e) {
+            log.error(ERROR_SECRET_INVALID_PARAM, e);
+            throw e;
+        } catch (InvalidRequestException e) {
+            log.error(ERROR_SECRET_REQUEST, e);
+            throw e;
+        } catch (ResourceNotFoundException e) {
+            log.error(ERROR_SECRET_RESOURCE_NOT_FOUND, e);
+            throw e;
+        }
+        
+        // Decrypts secret using the associated KMS CMK.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if (getSecretValueResult.getSecretString() != null) {
+            secret = getSecretValueResult.getSecretString();
+        }
+        
+        return new Gson().fromJson(secret, JsonObject.class);
     }
     
 }
--- src/main/java/com/meli/challenge/xmenvalidator/general/Constants.java
+++ src/main/java/com/meli/challenge/xmenvalidator/general/Constants.java
@@ -5,9 +5,28 @@ import java.util.List;
 
 public class Constants {
     
+    /**
+     * General
+     **/
+    
     //Final validation chain from dna
     public static final List<String> SEQUENCES = Arrays.asList("AAAA", "CCCC", "GGGG", "TTTT");
     //Min value for validate a chain
     public static final int MIN_VALUE_CROSS = 4;
+    
+    /**
+     * Messages Error
+     */
+    
+    public static final String ERROR_SECRET_DECRYPT = "Secrets Manager can't decrypt the protected secret text using " +
+            "the provided KMS key. Deal with the exception here, and/or rethrow at your discretion.";
+    public static final String ERROR_SECRET_SERVICE_FAIL = "An error occurred on the server side. Deal with the exception " +
+            "here, and/or rethrow at your discretion.";
+    public static final String ERROR_SECRET_INVALID_PARAM = "You provided an invalid value for a parameter. Deal with" +
+            " the exception here, and/or rethrow at your discretion.";
+    public static final String ERROR_SECRET_REQUEST = "You provided a parameter value that is not valid for the " +
+            "current state of the resource. Deal with the exception here, and/or rethrow at your discretion.";
+    public static final String ERROR_SECRET_RESOURCE_NOT_FOUND = " We can't find the resource that you asked for. Deal" +
+            " with the exception here, and/or rethrow at your discretion.";
     
 }
